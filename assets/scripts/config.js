d664ca922583f3fec456821a5f88f260c7eb1150
mrmcgown <mrmcgown@gmail.com>
2018-08-09 11:28:13 -0400

    removed slash at end of production url in config.js

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://ga-inventory-manager.herokuapp.com/',
+  production: 'https://ga-inventory-manager.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
